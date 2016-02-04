2f4a93c0ddae4eb34b91e2c6b90ccbf23f0377ff
Álvaro Velázquez Vico <uo232635@uniovi.es>
2016-02-04 17:41:17 +0100

    TurnBattleHandler

---
+++ TFG/Assets/Scripts/Character/BaseCharacter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseCharacter  {
+
+    public string name = "Nombre";
+    public string description = "Descripcion";
+
+    public string Nombre {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+}
---
+++ TFG/Assets/Scripts/TurnBattle/TurnBattleHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/* Se va a tratar de una máquina de estados
+*/
+public class TurnBattleHandler{
+
+    private const int START_BATTLE = 0;
+    private const int CHOOSE_FIGHTER = 1;
+    private const int FIGHTER_ACTION = 2;
+    private const int FIGHTER_ACTION_EFFECT = 3;
+    private const int CHOOSE_NEXT_ACTION = 4;
+    private const int FINISH_BATTLE = 5;
+
+    private List<BaseCharacter> players;
+    private List<BaseCharacter> enemigos;
+
+    public TurnBattleHandler()
+    {
+        players = new List<BaseCharacter>();
+        enemigos = new List<BaseCharacter>();
+    }
+
+}
