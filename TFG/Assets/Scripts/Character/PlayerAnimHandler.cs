d05e0c686ccd221180ca768514dc6187a7ff7de1
Álvaro Velázquez Vico <uo232635@uniovi.es>
2016-02-05 10:26:13 +0100

    PlayerAnimHandler y movimiento personaje

---
+++ TFG/Assets/Scripts/Character/PlayerAnimHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerAnimHandler {
+
+
+    public static string input_x = "input_x";
+    public static string input_y = "input_y";
+    public static string isRunning = "isRunning";
+    public static Animator Animator { get; set; }
+
+    public static void Estado_Correr_Parado(Vector2 movement)
+    {
+        if (movement != Vector2.zero)
+        {
+            Animator.SetFloat(PlayerAnimHandler.input_x, movement.x);
+            Animator.SetFloat(PlayerAnimHandler.input_y, movement.y);
+            Animator.SetBool(PlayerAnimHandler.isRunning, true);
+        }
+        else
+        {
+            Animator.SetBool(PlayerAnimHandler.isRunning, false);
+        }
+    }
+
+	
+}
+
+
---
+++ TFG/Assets/Scripts/Character/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    [Range(1,5)]
+    public float speed = 1.0f;
+    private Rigidbody2D rgb;
+
+    private float input_x;
+    private float input_y;
+    private bool isRunning;
+    private Vector2 movement;
+
+    private Animator anim;
+
+
+	// Use this for initialization
+	void Awake () {
+        rgb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        PlayerAnimHandler.Animator = anim;
+	}
+
+    void Update()
+    {
+        input_x = Input.GetAxisRaw("Horizontal") ;
+        input_y = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(input_x, input_y) * speed;
+
+        //Llamamos al handler de las animaciones del player que maneje Correr-Parado
+        PlayerAnimHandler.Estado_Correr_Parado(movement);    
+
+    }
+	
+
+	void FixedUpdate ()
+    {
+        Movement();
+	}
+
+
+    void Movement()
+    {
+        rgb.MovePosition(rgb.position + movement * Time.fixedDeltaTime);
+    }
+}
