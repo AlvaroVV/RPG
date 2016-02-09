23d7a904c4037a7e17689062686cb296a4b7c753
Álvaro Velázquez Vico <uo232635@uniovi.es>
2016-02-09 13:43:45 +0100

    Modal Panel Part 1
    Sobre el minuto 40 del tutorial

---
+++ TFG/Assets/Scripts/UI/BringToFront.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*Script que lleva el Panel al último para que se visualice el primero.
+*/
+public class BringToFront : MonoBehaviour {
+
+	void onEnable()
+    {
+        transform.SetAsLastSibling();
+    }
+}
