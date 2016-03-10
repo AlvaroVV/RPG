8ecf9cda631e7c33758a8a8f86e565dcaafd6d86
Álvaro Velázquez Vico <uo232635@uniovi.es>
2016-03-10 16:42:09 +0100

    Arreglado Bugg de nombres de TurnPanel con enemigos

--- TFG/Assets/Scripts/TurnBattle/StartFightState.cs
+++ TFG/Assets/Scripts/TurnBattle/StartFightState.cs
@@ -40,23 +40,22 @@ public class StartFightState : IState
     private void InstantiateEnemy(EnemyData enemyData,Transform transform)
     {
         GameObject enemyObject = Resources.Load("Enemies/EnemyFighter") as GameObject;
-        EnemyFighter enemyFighter = enemyObject.GetComponent<EnemyFighter>();
+        GameObject enemyInstantiate = GameObject.Instantiate(enemyObject, transform.position, Quaternion.identity) as GameObject;
+
+        EnemyFighter enemyFighter = enemyInstantiate.GetComponent<EnemyFighter>();
 
         enemyFighter.setEnemyProperties(enemyData);
-
-        GameObject enemyInstantiate = GameObject.Instantiate(enemyObject, transform.position, Quaternion.identity) as GameObject;
         enemyInstantiate.transform.parent = tb.transform;
-
-        tb.enemyFighters.Add(enemyInstantiate);
 
         enemyInstantiate.name = ChooseName(enemyData.Name);
         enemyFighter.fighterName = enemyInstantiate.name;
 
+        tb.enemyFighters.Add(enemyInstantiate);
         tb.stackTurnfighter.Add(enemyFighter);
     }
 
     private string ChooseName(string name)
     {
-        return name+"_" + tb.enemyFighters.Where( fighter => fighter.GetComponent<EnemyFighter>().fighterName.StartsWith(name)).Count();
+        return name+"_" + (tb.enemyFighters.Where( fighter => fighter.GetComponent<EnemyFighter>().fighterName.StartsWith(name)).Count() +1);
     }
 }
